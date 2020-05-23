48d88781e5eb7cbcdd254f1179af7102f8415510
Robert Kulesza <kuleszamusician@gmail.com>
2020-05-23 17:56:46 +0000

    Req 1a Fulfilled

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 const Bot = new Discord.Client();
 const Token = "Njk4NDIyMDk0NTcyMjkwMDk4.XqdICQ.rTvA62jCxiJDpmk0wl6o4DoPKOM";
 
+const Interpreter = require("./modules/interpreter_module").start(); 
+
 Bot.login(Token);
 Bot.on('ready', function(){
   console.log("logged in");
 });
 
 Bot.on('message', function(msg){
-  var split = msg.content.split(" ");
-  if(split[0] == "+compilebot"){
-    msg.reply("Testing 123...");
+  if(Interpreter.isCBCommand(msg.content)){
+    msg.reply("CompileBot is Responding.");
   }
 });
